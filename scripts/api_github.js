5866807c585390909d3e79e9fd032a092c16f4b2
PatricioOsorio <patriciomiguel_12@hotmail.com>
2022-04-09 19:48:50 -0500

    Correccion api github

--- scripts/api_github.js
+++ scripts/api_github.js
@@ -7,7 +7,7 @@ export default function apiGithub() {
         'https://api.github.com/users/PatricioOsorio/repos'
       );
       let json = await (res.ok ? res.json() : Promise.reject(res));
-      
+
       console.log(json);
 
       const $template = d.querySelector('.card__template').content;
@@ -16,13 +16,23 @@ export default function apiGithub() {
 
       json.forEach((el) => {
         $template.querySelector('.card__title').textContent = el.name;
-        $template.querySelector(
-          '.card__website'
-        ).href = `https://patricioosorio.github.io/${el.name}`;
+
+        if (el.has_pages) {
+          $template.querySelector(
+            '.card__website'
+          ).href = `https://patricioosorio.github.io/${el.name}`;
+          $template.querySelector(
+            '.card__website'
+          ).target = `_blank`;
+        }
 
         $template.querySelector('.card__repository').href = el.html_url;
-        $template.querySelector('.card__created').textContent = new Date(el.created_at).toLocaleDateString("es-MX");
-        $template.querySelector('.card__updated').textContent = new Date(el.updated_at).toLocaleDateString("es-MX");
+        $template.querySelector('.card__created').textContent = new Date(
+          el.created_at
+        ).toLocaleDateString('es-MX');
+        $template.querySelector('.card__updated').textContent = new Date(
+          el.updated_at
+        ).toLocaleDateString('es-MX');
         $template.querySelector('.card__language').textContent = el.language;
 
         let $clone = d.importNode($template, true);
